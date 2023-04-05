dc309c692251959c0fec2dc0a45d50f1370d9c79
Emmanuel Kurinaah <kurinaah@hartford.edu>
2023-04-05 14:28:54 -0400

    finalWork

--- Matrix.java
+++ Matrix.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
 
 public class Matrix {
-    private int[][] matrix; // 2D array to store the matrix
-    private int matNumber;  //Data for the matrix size entered by user
+  private int[][] matrix; // 2D array to store the matrix
+  private int matNumber;  //Data for the matrix size entered by user
 
-    public Matrix(int matNumber) {
-        this.matNumber = matNumber;
-        this.matrix = new int[matNumber][matNumber];
-        for (int row = 0; row < matNumber; row++) {
-          for (int column = 0; column < matNumber; column++) {
-              System.out.print(matrix[row][column] + "       ");
+  //Starting point for square matrix to be printed with 0s after matrix initialization
+  public Matrix(int matNumber) {
+    this.matNumber = matNumber;
+    this.matrix = new int[matNumber][matNumber];
+      for (int row = 0; row < matNumber; row++) {
+        for (int column = 0; column < matNumber; column++) {
+           System.out.print(matrix[row][column] + "       ");
           }
-          System.out.println();
+          System.out.println(); //Moves to next line after each iteration of row
       }
     }
 
-    // Method to swap the values at two different positions in the matrix
-    private void swap(int x1, int y1, int x2, int y2) {
-        int temp = matrix[x1][y1];
-        matrix[x1][y1] = matrix[x2][y2];
-        matrix[x2][y2] = temp;
+    /*  Method to swap the values at two different positions in the matrix
+        Takes in oringinal postions of matrices to be swapped
+    */
+  private void swap(int x1, int y1, int x2, int y2) {
+    int matPos = matrix[x1][y1];
+      matrix[x1][y1] = matrix[x2][y2];
+       matrix[x2][y2] = matPos;
     }
 
     // Method to populate the matrix with numbers from 1 to the square of user entered number
-    public void populateMatrix() {
-        int count = 1;
-        for (int row = 0; row < matNumber; row++) {
-            for (int column = 0; column < matNumber; column++) {
-                matrix[row][column] = count++;  //Assigning sequential numbers
-            }
+  public void populateMatrix() {
+    int count = 1;
+      for (int row = 0; row < matNumber; row++) {
+        for (int column = 0; column < matNumber; column++) {
+          matrix[row][column] = count++;  //Assigning numbers in sequence to square of matNumber
+          }
         }
     }
 
@@ -36,38 +39,26 @@ public class Matrix {
     public void printMatrix() {
       System.out.println("\nMatrix with highlighted secondary diagonal:");
       for (int row = 0; row < matNumber; row++) {
-          for (int column = 0; column < matNumber; column++) {
-              if (row + column == matNumber - 1) { // check if element is on the secondary diagonal
-                  System.out.print("\033[43m" + matrix[row][column] + "\033[0m\t");
-              } else {
-                  System.out.print(matrix[row][column] + "\t");
+        for (int column = 0; column < matNumber; column++) {
+          if (row + column == matNumber - 1) { // check if number is on secondary diagonal
+             System.out.print("\033[43m" + matrix[row][column] + "\033[0m\t");  //Color code secondary diagonal number in yellow
+
+            } else {
+                System.out.print(matrix[row][column] + "\t");
               }
           }
-          System.out.println();
+        System.out.println();
       }
   }
 
+  //Flips positions of matrices
   public void flipMatrix() {
-        for (int row = 0; row < matNumber; row++) {
-            for (int column = 0; column < matNumber / 2; column++) {
-                if (column == matNumber / 2 - 1 && matNumber % 2 == 1) { // skip highlighted number in middle column
-                    continue;
-                }
-                int temp = matrix[row][column];
-                matrix[row][column] = matrix[row][matNumber - column - 1];
-                matrix[row][matNumber - column - 1] = temp;
-            }
-        }
+    int l = matrix.length;  //Calls matrix size
+    //Condition runs from first row till it can't go through the gate (last)
+    for (int row = 0; row < l; row++) {
+      for (int col = 0; col < l-1-row; col++) {
+        swap(row, col, l-1-row, l-1-col);
+      }
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the matNumber of the matrix: ");
-        int matNumber = scanner.nextInt();
-
-        Matrix matrix = new Matrix(matNumber);
-        matrix.populateMatrix();
-        matrix.printMatrix();
-        matrix.flipMatrix();
-        matrix.printMatrix();
-    }
+  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -11,30 +11,5 @@ public class RunMatrix {
         runIt.printMatrix();
         runIt.flipMatrix();
         runIt.printMatrix();
-
-        /*
-        int maxNumber = 0;
-        int i,j,m,k = 0;
-        System.out.println();
-
-        maxNumber = matNumber*matNumber;
-        for (i = maxNumber; i >= (maxNumber - matNumber + 2); i-- ) {
-          System.out.print(i + "  ");
-        }
-        System.out.print(matNumber);
-        System.out.print(i);
-
-        for (m = 2; m <= matNumber; m++) {
-          for (j=i-1; j >= 1; j--){
-           if ( j == (matNumber * m) - (m - 1) ) {
-            System.out.print("a" + "  ");
-          }
-            else {
-              System.out.print(j + "  ");
-            }
-        }
-        System.out.println();
-      }
-      */
     }
 }
