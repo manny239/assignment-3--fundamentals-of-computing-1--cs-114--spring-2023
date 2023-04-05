61dd7a8ccf11e2d71cf4d3b35c95c9bc20f6b804
Emmanuel Kurinaah <kurinaah@hartford.edu>
2023-04-05 14:39:52 -0400

    Update RunMatrix.java

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
 public class RunMatrix {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a positive integer to print square matrix: ");
-        int matNumber = input.nextInt();
-
-        Matrix runIt = new Matrix(matNumber);
-        runIt.populateMatrix();
-        runIt.printMatrix();
-        runIt.flipMatrix();
-        runIt.printMatrix();
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a positive integer to print square matrix: ");
+    int matNumber = input.nextInt();
+    Matrix runIt = new Matrix(matNumber);
+    runIt.populateMatrix();
+    runIt.printMatrix();
+    runIt.flipMatrix();
+    runIt.printMatrix();
     }
 }
